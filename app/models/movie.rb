525cc74a58725bb1a3f200e6ae4c37c8e022491c
yoyo <yuziyao2007@163.com>
2017-04-06 20:45:47 +0800

    implement movie relation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,6 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   belongs_to :user
   has_many :reviews
+  has_many :movie_relationships
+  has_many :members, through: :movie_relationships, source: :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406052227) do
+ActiveRecord::Schema.define(version: 20170406082444) do
+
+  create_table "movie_relationships", force: :cascade do |t|
+    t.integer  "movie_id"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
