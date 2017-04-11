8d15f99c449bedcfaa3c4f054834c29d93363a86
yoyo <yuziyao2007@163.com>
2017-04-11 10:53:23 +0800

    implement my reviews

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
   namespace :account do
     resources :movies
+    resources :reviews
   end
   root 'movies#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
